49f4c018c1d023db3696d318dc08a3880c22d47e
BDMajora <bdmajora@protonmail.com>
2024-08-11 11:12:35 -0600

    RenderEngineMixin now loads

--- src/main/java/bdmajora/backport/mixin/RenderEngineMixin.java
+++ src/main/java/bdmajora/backport/mixin/RenderEngineMixin.java
@@ -3,37 +3,36 @@ package bdmajora.backport.mixin;
 import bdmajora.backport.block.DynamicTextures.DynamicTextureCrimsonStem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.render.RenderEngine;
+import net.minecraft.client.render.dynamictexture.DynamicTexture;
 import net.minecraft.client.render.texturepack.TexturePack;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.Shadow;
 
 import java.util.List;
 
 import static bdmajora.backport.backport.MOD_ID;
 
-@Mixin(value = RenderEngine.class, remap = false) // Corrected the attribute to 'remap'
+@Mixin(value = RenderEngine.class, remap = false)
 public abstract class RenderEngineMixin {
 
-	@Shadow
-	private List<Object> dynamicTextures;
+	@Shadow @Final
+	private List<DynamicTexture> dynamicTextures;
 
-	@Shadow
-	public abstract TexturePack getTexturePack();
-
-	@Shadow
-	public abstract Minecraft getMinecraft();
+	@Shadow @Final
+	private Minecraft mc;
 
 	@Inject(method = "initDynamicTextures", at = @At("TAIL"))
 	private void addCustomDynamicTextures(CallbackInfo ci) {
-		Minecraft mc = getMinecraft();
-		TexturePack texturePack = getTexturePack();
+		String texturePath = "/assets/" + MOD_ID + "/textures/block/crimson_stem.png";
+		TexturePack texturePack = mc.texturePackList.getHighestPriorityTexturePackWithFile(texturePath);
 
-		String texturePath = MOD_ID + ":block/crimson_stem";
-
-		DynamicTextureCrimsonStem crimsonStemTexture = new DynamicTextureCrimsonStem(mc, texturePack);
-		dynamicTextures.add(crimsonStemTexture);
+		if (texturePack != null) {
+			DynamicTextureCrimsonStem crimsonStemTexture = new DynamicTextureCrimsonStem(mc, texturePack);
+			dynamicTextures.add(crimsonStemTexture);
+		}
 	}
 }
