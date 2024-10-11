de62cd208425af975c5be7f1fbecdb83d24043a4
bdmajora <bdmajora@protonmail.com>
2024-10-11 12:50:08 -0600

    disabled rendering of mushrooms

--- src/main/java/bdmajora/backport/entity/models/ModelBogged.java
+++ src/main/java/bdmajora/backport/entity/models/ModelBogged.java
@@ -11,7 +11,7 @@ public class ModelBogged extends ModelSkeleton {
 		// Initialize skeleton parts from ModelSkeleton
 		super();
 
-		// Mushrooms attached to the head (0-1, bottom right), forming X shape
+		// Mushrooms initialization (if needed, but won't be rendered)
 		this.mushrooms[0] = new Cube(50, 16, 64, 32);
 		this.mushrooms[0].addBox(-3.0F, -3.5F, 0.0F, 6, 4, 0);
 		this.mushrooms[0].setRotationPoint(3.0F, -7.5F, 3.0F);
@@ -22,18 +22,16 @@ public class ModelBogged extends ModelSkeleton {
 		this.mushrooms[1].setRotationPoint(3.0F, -7.5F, 3.0F);
 		this.mushrooms[1].rotateAngleY = -0.7854F;
 
-		// Mushrooms 2 and 3 forming an X shape, now on the back of the head
 		this.mushrooms[2] = new Cube(50, 27, 64, 32);
 		this.mushrooms[2].addBox(-3.0F, -5.0F, 0.0F, 6, 5, 0);
-		this.mushrooms[2].setRotationPoint(-3.0F, -6.0F, 6.0F); // Moved farther back
-		this.mushrooms[2].rotateAngleZ = 0.7854F; // X shape, rotated along the Z-axis
+		this.mushrooms[2].setRotationPoint(-3.0F, -6.0F, 6.0F);
+		this.mushrooms[2].rotateAngleZ = 0.7854F;
 
 		this.mushrooms[3] = new Cube(50, 27, 64, 32);
 		this.mushrooms[3].addBox(-3.0F, -5.0F, 0.0F, 6, 5, 0);
-		this.mushrooms[3].setRotationPoint(3.0F, -6.0F, 6.0F); // Lowered and behind the head, mirrored
-		this.mushrooms[3].rotateAngleZ = -0.7854F; // X shape, rotated along the Z-axis
+		this.mushrooms[3].setRotationPoint(3.0F, -6.0F, 6.0F);
+		this.mushrooms[3].rotateAngleZ = -0.7854F;
 
-		// Mushrooms 4 and 5 (unchanged)
 		this.mushrooms[4] = new Cube(50, 22, 64, 32);
 		this.mushrooms[4].addBox(-3.0F, -2.5F, 0.0F, 6, 4, 0);
 		this.mushrooms[4].setRotationPoint(-3.0F, -10.0F, -3.0F);
@@ -52,12 +50,7 @@ public class ModelBogged extends ModelSkeleton {
 		// Call render for skeleton body parts
 		super.render(limbSwing, limbYaw, limbPitch, headYaw, headPitch, scale);
 
-		// Render mushrooms, except for mushrooms[2] and mushrooms[3]
-		for (int i = 0; i < mushrooms.length; i++) {
-			if (i != 2 && i != 3) {  // Skip mushrooms 2 and 3
-				mushrooms[i].render(scale);
-			}
-		}
+		// Do not render mushrooms (turned off)
 	}
 
 	@Override
